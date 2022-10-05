60c26b2026aa0906593176d231e320bbc0b718b1
arekjg <arekjg@gmail.com>
2022-10-05 22:33:39 +0200

    code refactoring

--- hotel_management.cpp
+++ hotel_management.cpp
@@ -1,6 +1,5 @@
 ﻿#include <iostream>
 #include <string>
-#include <map>
 using namespace std;
 const int NUM_OF_ITEMS = 7;
 
@@ -10,7 +9,7 @@ const int NUM_OF_ITEMS = 7;
 //		used for loops to store data in variables
 //		used for loops to prompt text in terminal instead of writing everything in file
 //		added sum function which calculates final price of purchased goods
-//		made the app universal, so it's possible to change freely number of items and their names without changing anything in the later code
+//		made the app universal, so it's possible to change freely number of items and their names without changing anything in later code
 
 int sum(int prices[])
 {
@@ -110,119 +109,7 @@ int main()
 			}
 		}
 
-		//	case 7:
-//		cout << "\n\t\tDetails of sales and collection ";
-//		cout << "\n\nNumber of rooms we had: " << Qrooms;
-//		cout << "\n\nNumber of rooms we gave for rent: " << Srooms;
-//		cout << "\n\nRemaining rooms: " << Qrooms - Srooms;
-//		cout << "\n\nTotal rooms collection for the day: " << Total_rooms;
-//		cout << "\n\nTotal food colletion for the day: " << Total_foods;
-
-
-
-		// ****************
-		
-		//switch (choice)
-		//{
-		//	case 1:
-		//		cout << "\nEnter the number of rooms you want: ";
-		//		cin >> quant;
-		//		if (Qrooms - Srooms >= quant)
-		//		{
-		//			Srooms = Srooms + quant;
-		//			Total_rooms = Total_rooms + quant * 1200;
-		//			cout << "\n\n\t\t" << quant << " room/rooms have been alloted to you";
-		//		}
-		//		else
-		//		
-		//			cout << "\n\tOnly " << Qrooms - Srooms << " rooms remaining in hotel";
-		//			break;
-		//		
-		//	case 2:
-		//		cout << "\nEnter the pasta quantity you want: ";
-		//		cin >> quant;
-		//		if (Qpasta - Spasta >= quant)
-		//		{
-		//			Spasta = Spasta + quant;
-		//			Total_pasta = Total_pasta + quant * 250;
-		//			cout << "\n\n\t\t" << quant << " pasta is the order";
-		//		}
-		//		else
-		//		
-		//			cout << "\n\tOnly " << Qpasta - Spasta << " pasta remaining in hotel";
-		//			break;
-		//		
-		//	case 3:
-		//		cout << "\nEnter the burger quantity you want: ";
-		//		cin >> quant;
-		//		if (Qburger - Sburger >= quant)
-		//		{
-		//			Sburger = Sburger + quant;
-		//			Total_burger = Total_burger + quant * 120;
-		//			cout << "\n\n\t\t" << quant << " burger is the order";
-		//		}
-		//		else
-		//		
-		//			cout << "\n\tOnly " << Qburger - Sburger << " burger remaining in hotel";
-		//			break;
-		//		
-		//	case 4:
-		//		cout << "\nEnter the noodles quantity you want: ";
-		//		cin >> quant;
-		//		if (Qnoodles - Snoodles >= quant)
-		//		{
-		//			Snoodles = Snoodles + quant;
-		//			Total_noodles = Total_noodles + quant * 140;
-		//			cout << "\n\n\t\t" << quant << " noodles is the order";
-		//		}
-		//		else
-		//		
-		//			cout << "\n\tOnly " << Qnoodles - Snoodles << " noodles remaining in hotel";
-		//			break;
-		//		
-		//	case 5:
-		//		cout << "\nEnter the shake quantity you want: ";
-		//		cin >> quant;
-		//		if (Qshake - Sshake >= quant)
-		//		{
-		//			Sshake = Sshake + quant;
-		//			Total_shake = Total_shake + quant * 120;
-		//			cout << "\n\n\t\t" << quant << " shake is the order";
-		//		}
-		//		else
-		//		
-		//			cout << "\n\tOnly " << Qshake - Sshake << " shake remaining in hotel";
-		//			break;
-		//		
-		//	case 6:
-		//		cout << "\nEnter the chicken quantity you want: ";
-		//		cin >> quant;
-		//		if (Qchicken - Schicken >= quant)
-		//		{
-		//			Schicken = Schicken + quant;
-		//			Total_chicken = Total_chicken + quant * 180;
-		//			cout << "\n\n\t\t" << quant << " chicken is the order";
-		//		}
-		//		else
-		//		
-		//			cout << "\n\tOnly " << Qchicken - Schicken << " chicken remaining in hotel";
-		//			break;
-		//		
-		//	case 7:
-		//		cout << "\n\t\tDetails of sales and collection ";
-		//		cout << "\n\nNumber of rooms we had: " << Qrooms;
-		//		cout << "\n\nNumber of rooms we gave for rent: " << Srooms;
-		//		cout << "\n\nRemaining rooms: " << Qrooms - Srooms;
-		//		cout << "\n\nTotal rooms collection for the day: " << Total_rooms;
-		//		cout << "\n\nTotal food colletion for the day: " << Total_foods;
-
-		//	case 8:
-		//		exit(0);
-		//default:
-		//	cout << "\nPlease select the numbers mentioned above";
-		//	break;
-		//}
-	goto menu;
+	goto menu;		// jump to menu
 
 	return 0;
 }
